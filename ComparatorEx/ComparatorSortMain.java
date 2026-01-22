90acb03416a796a1571b8eb6567b0ccfe671b8a6
ShreyasShingare <shreyas.shingare@gmail.com>
2026-01-22 12:17:56 +0000

    added comparator example

---
+++ ComparatorEx/ComparatorSortMain.java
@@ -0,0 +1,37 @@
+package ComparatorEx;
+import java.util.*;
+
+public class ComparatorSortMain {
+	public static void main(String[] args) {
+		
+		Comparator<Integer> com = new Comparator<>() {
+
+			@Override
+			public int compare(Integer i, Integer j) {
+				
+				if(i%10 > j%10) {
+					return 1;
+				}
+				else {
+					return -1;
+				}
+		
+			}
+			
+		};
+		
+		List<Integer> nums = new ArrayList<>();
+		
+		nums.add(43);
+		nums.add(31);
+		nums.add(72);
+		nums.add(29);
+		
+		Collections.sort(nums, com);
+		
+		System.out.println(nums);
+		
+		
+	}
+
+}
